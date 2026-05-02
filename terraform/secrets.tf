f97ec09a14b4fa8a8420358cd0fa70928e27b5e3
DileepKumarReddySathi <dileep@example.com>
2026-05-02 16:13:53 +0530

    Remove legacy AWS files

--- terraform/secrets.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_secretsmanager_secret" "db_password" {
-  name = "${var.project_name}-db-password"
-}
-
-resource "aws_secretsmanager_secret_version" "db_password" {
-  secret_id     = aws_secretsmanager_secret.db_password.id
-  secret_string = var.db_password
-}
